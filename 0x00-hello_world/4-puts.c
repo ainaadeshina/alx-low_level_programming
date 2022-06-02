eae6dc9c7e649f372dd3676b4c78944d07b8a142
Adeshina O. Aina <ainaadeshina@gmail.com>
2022-06-02 11:57:37 -0700

    all files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints "programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, otherwise non-zero
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
