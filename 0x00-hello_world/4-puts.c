7ca0210d651dfb2fc19760bf7116674133f2bd9c
Adeshina O. Aina <ainaadeshina@gmail.com>
2022-06-02 12:29:14 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints "programming is like building a multilingual puzzle
- * Return: 0 if exited properly, otherwise non-zero
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
