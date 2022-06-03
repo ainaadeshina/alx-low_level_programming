def8e9a03a54097ae1d7c01e3cc234f5792b02bc
Adeshina O. Aina <ainaadeshina@gmail.com>
2022-06-03 15:45:11 -0700

    new

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,18 +5,15 @@
  */
 int main(void)
 {
-	char l;
 	int n;
 
 	for (n = 0; n <= 9; n++)
-	{
 		putchar((n % 10) + '0');
 		if (n == 9)
 			continue;
 
 		putchar(',');
 		putchar('');
-	}
 
 	putchar('\n');
 
