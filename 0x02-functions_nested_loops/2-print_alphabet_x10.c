cf3243ca3799eb17621cb758d48a88286045528e
Adeshina O. Aina <ainaadeshina@gmail.com>
2022-06-07 13:31:03 -0700

    add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+# include "main.h"
+/**
+ * main - prints the alphabets 10 times
+ * Return: 10x a to z
+ */
+void print_alphabet_x10(void)
+{
+	int l, n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+		}
+		n++;
+		_putchar('\n');
+	}
+}
