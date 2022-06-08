82b2252971f95d8b23de40d8e01c534f79e49adc
Adeshina O. Aina <ainaadeshina@gmail.com>
2022-06-07 19:52:41 -0700

    add

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,36 +6,43 @@
  */
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int a = 0, r, b;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	while (a <= n)
 	{
-		for (num = 0; num <= n; num++)
+		for (b = 0; b <= n; b++)
 		{
-			_putchar('0');
-			
-			for (mult =1; mult <= n; mult++)
+			r = a * b;
+			if (b == 0)
+				_putchar('0' + r);
+			else if (r < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + r);
+			}
+			else if (r < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + r / 10);
+				_putchar('0' + r % 10);
+			}
+			else
+			{
+				_putchar('0' + r / 100);
+				_putchar('0' + (r - 100) / 10);
+				_putchar('0' + r % 10);
+			}
+			if (b < n)
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
+		a++;
 	}
 }
