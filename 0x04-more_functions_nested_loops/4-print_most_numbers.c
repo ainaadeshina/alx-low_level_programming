6d9cc8abeaa5b53a8639ed504d2c78bfb1e28c49
Adeshina O. Aina <ainaadeshina@gmail.com>
2022-06-09 19:54:21 -0700

    chng

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-# include "main.h"
+#include "main.h"
 /**
  * print_most_numbers - prints numbers 0 to 9,excluding 2 and 4.
  * Description; Can use _putchar only twice
@@ -10,9 +10,9 @@ int i;
 for (i = 0; i <= 9; i++)
 {
 if ((i == 2) || (i == 4))
-	continue;
+continue;
 else
-	_putchar(i + '0');
+_putchar(i + '0');
 i++;
 }
 _putchar('\n');
