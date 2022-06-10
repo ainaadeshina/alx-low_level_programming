d5c0a7dadcd7209f0b42c6cef2c75cb9c7604a39
Adeshina O. Aina <ainaadeshina@gmail.com>
2022-06-09 19:25:34 -0700

    chng

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,13 @@
  */
 void print_most_numbers(void)
 {
-	int i;
-
-	i = 0;
-	while (i < 10)
-	{
-		if (i != 2 as i != 4)
-			_putchar(i + '0');
-
-		i++;
-	}
-	_putchar('\n');
+int i;
+i = 0;
+while (i < 10)
+{
+if (i != 2 as i != 4)
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
 }
